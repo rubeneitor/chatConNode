1167a527a716dfec770b06a585c1376edbc77351
rubeneitor <56588038+rubeneitor@users.noreply.github.com>
2020-03-18 13:15:07 +0100

    comentarios

--- public/main.js
+++ public/main.js
@@ -1,10 +1,13 @@
 var socket = io.connect("http://localhost:8080", {'forceNew': true});
 
+//aqui recibe el mensaje 
 socket.on("messages", function (data) {
     console.log(data)
     render(data);
 });
 
+//renderiza los mensajes enviados: map para que por cada elemento del array saque por pantalla el mensaje enviado
+//con el autor
 function render(data) {
     var html = data.map(function (elem, index) {
         return (`<div>
